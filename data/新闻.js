81ecb6be8cd4b138934dedf69b955fd20ce19ed5
Hotomoderato <48020748+Hotomoderato@users.noreply.github.com>
2021-05-09 23:43:19 +0100

    Version1
    V1.0

---
+++ data/新闻.js
@@ -0,0 +1,86 @@
+/*
+ * @Descripttion: 
+ * @Date: 2021-05-07 19:46:31
+ */
+var newsData = [
+  {
+    "year": "2000",
+    "title": "I. China-ASEAN Free Trade Area completed",
+    "details": "On 1 January, the China-ASEAN Free Trade Area (FTA) was completed. It is currently the largest FTA composed entirely of developing countries, covering a population of 1.9 billion people, a GDP of US$6 trillion and a trade volume of US$4.5 trillion. The completion of the China-ASEAN FTA has significantly improved the efficiency of the flow of production factors such as capital, resources, technology and talents between the two sides, providing an unprecedentedly favourable environment for expanding trade and investment cooperation, and is an important milestone in the history of China-ASEAN relations."
+  },
+  {
+    "year": "2001",
+    "title": "II. Difficulties in rebuilding Haiti after a strong earthquake",
+    "details": "??On 12 January, a powerful earthquake measuring 7.3 on the Richter scale struck the Caribbean island nation of Haiti, killing a rough estimate of more than 200,000 people and causing severe damage to infrastructure, making it the worst disaster in Haiti in 200 years. In October, a cholera epidemic broke out in Haiti, spreading throughout the country within weeks. By 22 December, the cholera epidemic had killed nearly 2,600 people and more than 120,000 were receiving treatment. Earthquakes, hurricanes, cholera and political turmoil have made the humanitarian situation in Haiti increasingly dire."
+  },
+  {
+    "year": "2002",
+    "title": "Security situation in Northeast Asia remains tense",
+    "details": "??On 26 March, the South Korean guard ship Cheonan exploded and sank in the western waters of the Korean peninsula (Yellow Sea), with only 58 of its 104 officers and men surviving. On 23 November, the two Koreas exchanged artillery fire near the disputed \"Northern Limit Line\", killing four people on the Korean side. From 28 November to 1 December, South Korea and the United States held joint military exercises in the western waters of the Korean peninsula with the participation of the USS George Washington, a nuclear-powered aircraft carrier, and on 3 December, the largest-ever joint military exercise between Japan and the United States took place in the waters around Japan. The security situation in Northeast Asia has remained tense for a year."
+  },
+  {
+    "year": "2003",
+    "title": "Icelandic volcanic ash cripples European aviation",
+    "details": "??On 14 April, a volcano erupted beneath the Eyjafjalla ice cap in Iceland, sending ash drifting eastward and southward, accumulating over Norway, Poland, Germany, France, Belgium, the United Kingdom and Russia, causing air traffic in Europe to be paralysed for a time and affecting global air transport. Within a week of the eruption, some 100,000 flights were cancelled in Europe, with economic losses amounting to billions of euros. The volcanic eruption, which lasted for about 40 days, also had a serious impact on Iceland's livestock and tourism industries."
+  },
+  {
+    "year": "2004",
+    "title": "US Gulf of Mexico oil spill causes ecological disaster",
+    "details": "??On 20 April, the Deepwater Horizon drilling rig leased by BP exploded and sank in the waters of the Gulf of Mexico near Louisiana, killing 11 people and spilling a large amount of crude oil. On 19 September, the US government announced that the oil spill had been permanently plugged. In the meantime, more than four million barrels of crude oil flowed into the sea, creating the worst oil spill in US history and causing serious damage to the ecology of the Gulf of Mexico."
+  },
+  {
+    "year": "2005",
+    "title": "Emerging countries rise in global economic status",
+    "details": "??On April 25, the Development Committee, the World Bank's decision-making body, voted to shift 3.13 percentage points of voting weight from developed to developing countries, raising the overall voting weight of developing countries to 47.19%. The IMF will transfer more than 6% of its quota to underrepresented countries, including emerging countries, by 2012. The Bank and the IMF have made successive breakthroughs in governance reform, reflecting the growing economic power of developing countries. China has become the third largest shareholder of the Bank and the third largest contributor to the IMF, gaining a greater voice in the two institutions and further enhancing its international standing."
+  },
+  {
+    "year": "2006",
+    "title": "European sovereign debt crisis spreads",
+    "details": "??On 27 April, the international rating agency Standard & Poor's downgraded Greece's credit rating again. On May 2, the eurozone launched a bailout mechanism for Greece; in November, the debt-ridden Ireland also had to accept external assistance; the market also fears that Portugal, Spain and Belgium will be the next victims. In response to the debt crisis, many countries in the eurozone were forced to adopt fiscal austerity policies, triggering strong public discontent."
+  },
+  {
+    "year": "2007",
+    "title": "World's first artificial single-cell organism born",
+    "details": "??On 20 May, a team led by US geneticist Craig Venter reported in the US journal Science that they had grown the world's first single-celled bacteria controlled entirely by an artificial genome. The achievement is a key step towards the artificial creation of simple life forms and will help advance research into new biofuel synthesis processes. The achievement promises to usher in a new era of genetic engineering."
+  },
+  {
+    "year": "2008",
+    "title": "Chilean mine rescue miracle",
+    "details": "?On 5 August, 33 miners were trapped 700 metres below the surface of a copper mine in San Jose, Chile, after a cave-in, surviving in difficult conditions and uniting to save themselves. Chile launched a national rescue operation with the help of the international community. After more than two months of efforts, all the trapped miners were rescued on 13 October. The scale and difficulty of the rescue, and the length of time the miners survived underground, were unprecedented in the world, creating a miracle in the history of mine rescue."
+  },
+  {
+    "year": "2009",
+    "title": "X. US quantitative easing policy raises questions in other countries",
+    "details": "??On 3 November, the US Federal Reserve launched its second round of quantitative easing monetary policy, namely the purchase of US$600 billion of long-term US Treasury bonds by the end of June 2011, to stimulate the US economic recovery. This policy has sparked widespread questions and criticism around the world. Critics believe that the actual effect of the Fed's \"printing of money\" to stimulate economic recovery is unpredictable and will lead to a flood of liquidity and devaluation of the US dollar, a rise in commodity prices and an influx of \"hot money\" to other countries, endangering international economic and financial stability and global economic recovery. Economic recovery"
+  },
+  {
+    "year": "2010",
+    "title": "US Gulf of Mexico oil spill causes ecological disaster",
+    "details": "??On 20 April, the Deepwater Horizon drilling rig leased by BP exploded and sank in the waters of the Gulf of Mexico near Louisiana, killing 11 people and spilling a large amount of crude oil. On 19 September, the US government announced that the oil spill had been permanently plugged. In the meantime, more than four million barrels of crude oil flowed into the sea, creating the worst oil spill in US history and causing serious damage to the ecology of the Gulf of Mexico."
+  },
+  {
+    "year": "2011",
+    "title": "Emerging countries rise in global economic status",
+    "details": "??On April 25, the Development Committee, the World Bank's decision-making body, voted to shift 3.13 percentage points of voting weight from developed to developing countries, raising the overall voting weight of developing countries to 47.19%. The IMF will transfer more than 6% of its quota to underrepresented countries, including emerging countries, by 2012. The Bank and the IMF have made successive breakthroughs in governance reform, reflecting the growing economic power of developing countries. China has become the third largest shareholder of the Bank and the third largest contributor to the IMF, gaining a greater voice in the two institutions and further enhancing its international standing."
+  },
+  {
+    "year": "2012",
+    "title": "European sovereign debt crisis spreads",
+    "details": "??On 27 April, the international rating agency Standard & Poor's downgraded Greece's credit rating again. On May 2, the eurozone launched a bailout mechanism for Greece; in November, the debt-ridden Ireland also had to accept external assistance; the market also fears that Portugal, Spain and Belgium will be the next victims. In response to the debt crisis, many countries in the eurozone were forced to adopt fiscal austerity policies, triggering strong public discontent."
+  },
+  {
+    "year": "2013",
+    "title": "World's first artificial single-cell organism born",
+    "details": "??On 20 May, a team led by US geneticist Craig Venter reported in the US journal Science that they had grown the world's first single-celled bacteria controlled entirely by an artificial genome. The achievement is a key step towards the artificial creation of simple life forms and will help advance research into new biofuel synthesis processes. The achievement promises to usher in a new era of genetic engineering."
+  },
+  {
+    "year": "2014",
+    "title": "Chilean mine rescue miracle",
+    "details": "?On 5 August, 33 miners were trapped 700 metres below the surface of a copper mine in San Jose, Chile, after a cave-in, surviving in difficult conditions and uniting to save themselves. Chile launched a national rescue operation with the help of the international community. After more than two months of efforts, all the trapped miners were rescued on 13 October. The scale and difficulty of the rescue, and the length of time the miners survived underground, were unprecedented in the world, creating a miracle in the history of mine rescue."
+  },
+  {
+    "year": "2015",
+    "title": "X. US quantitative easing policy raises questions in other countries",
+    "details": "??On 3 November, the US Federal Reserve launched its second round of quantitative easing monetary policy, namely the purchase of US$600 billion of long-term US Treasury bonds by the end of June 2011, to stimulate the US economic recovery. This policy has sparked widespread questions and criticism around the world. Critics believe that the actual effect of the Fed's \"printing of money\" to stimulate economic recovery is unpredictable and will lead to a flood of liquidity and devaluation of the US dollar, a rise in commodity prices and an influx of \"hot money\" to other countries, endangering international economic and financial stability and global economic recovery. Economic recovery"
+  }
+]
